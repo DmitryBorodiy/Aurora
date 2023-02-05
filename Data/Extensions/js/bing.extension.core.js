5fcd1da35532d13d2cdb5f3db001d388d7c9b5a2
Dmitriy <winsoft.program@outlook.com>
2023-02-05 23:06:19 +0200

    data update

---
+++ Data/Extensions/js/bing.extension.core.js
@@ -0,0 +1,8 @@
+﻿document.getElementById("sb_form").style.visibility = "collapse";
+document.getElementsByClassName("b_scopebar")[0].style.position = "absolute";
+document.getElementsByClassName("b_scopebar")[0].style.top = "2px";
+document.getElementById("b_header").style.height = "60px";
+document.getElementsByClassName("b_logoArea")[0].style.visibility = "collapse";
+document.getElementById("mHamburger").style.visibility = "collapse";
+document.getElementById("id_sc").style.visibility = "collapse";
+document.getElementsByClassName("mic_cont")[0].remove();
