d2c5d8bd4916dd689dfea67909db50434f6123ba
Dmitriy <winsoft.program@outlook.com>
2023-09-27 19:53:42 +0300

    Update index.js

--- Web/index.js
+++ Web/index.js
@@ -8,10 +8,17 @@ import {
     SwatchRGB
 } from "https://unpkg.com/@fluentui/web-components@2.0.0";
 
+var navigationBar;
+var branding;
 var windowTheme = "Light";
 
 window.onload = (event) => {
+    navigationBar = document.getElementById("navigation-menu");
+    branding = document.getElementById("branding-div");
+
     ApplyTheme();
+
+    window.addEventListener("resize", Page_OnResize);
 };
 
 function ApplyTheme(){
@@ -34,4 +41,32 @@ function ApplyTheme(){
     catch(e){
         console.log(e);
     }
+}
+
+function Page_OnResize(){
+    try{
+        var pageWidth = getWindowWidth();
+
+        if(pageWidth < 700){
+            navigationBar.style.visibility = "collapse";
+            branding.style.left = "90px";
+        }
+        else{
+            navigationBar.style.visibility = "visible";
+            branding.style.left = "20px";
+        }
+    }
+    catch(e){
+        console.log(e);
+    }
+}
+
+function getWindowWidth() {
+    return Math.max(
+      document.body.scrollWidth,
+      document.documentElement.scrollWidth,
+      document.body.offsetWidth,
+      document.documentElement.offsetWidth,
+      document.documentElement.clientWidth
+    );
 }
